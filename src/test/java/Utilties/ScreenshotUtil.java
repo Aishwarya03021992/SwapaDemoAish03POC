481dcc28a70f568d2259c00bfc55cb28375a0e8e
AishwaryaK <k.aishwarya292@gmail.com>
2024-06-21 16:37:16 +0530

    extended report implementation

---
+++ src/test/java/Utilties/ScreenshotUtil.java
@@ -0,0 +1,26 @@
+package Utilties;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+
+import java.io.File;
+import java.io.IOException;
+
+public class ScreenshotUtil {
+    public static String captureScreenshot(WebDriver driver, String screenshotName) {
+        try {
+            TakesScreenshot ts = (TakesScreenshot) driver;
+            File source = ts.getScreenshotAs(OutputType.FILE);
+            String dest = System.getProperty("/Users/presidio/Documents/SWAPA POC/Automation/AutomationPOC/target") + "/snaps/" + screenshotName + ".png";
+            File destination = new File(dest);
+            FileUtils.copyFile(source, destination);
+            return dest;
+        } catch (IOException e) {
+            System.out.println("Exception while taking screenshot " + e.getMessage());
+            return e.getMessage();
+        }
+
+    }
+}
